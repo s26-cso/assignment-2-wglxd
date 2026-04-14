d7a5867872ea90d12aff78aa8bcae9a952ea702c
Tanush Choda <tanush.choda@gmail.com>
2026-04-14 17:45:09 +0530

    minor changes

--- q4/q4.c
+++ q4/q4.c
@@ -11,20 +11,20 @@ int main(){
     char op[MAX_OP_LEN];
     int num1;
     int num2;
-    while(1){
-        scanf("%5s %d %d", op, &num1, &num2);      /**Taking input, operation name, and then 2 numbers */
+    while(scanf("%5s %d %d", op, &num1, &num2) == 3){
+              /**Taking input, operation name, and then 2 numbers */
         char lib_name[LIB_NAME_LEN];              
         snprintf(lib_name, sizeof(lib_name), "./lib%s.so", op);   /*Writes into lib_name, writes the string of format lib%s.so*/
         void *handle = dlopen(lib_name, RTLD_LAZY);               /*Handle of the library, opening it in lazy mode*/
         if(handle==NULL){
             fprintf(stderr, "Error opening the library.\n");      /*If null, print to stderror*/
-            return 1;
+            continue;
         }
         operation_func operation = (operation_func) dlsym(handle, op);    /*dlsym is used to access anything by name, can be functions, global variables and constants*/
         if(operation == NULL){
             fprintf(stderr, "Error accessing the operation\n");           /*If null, print to stderr*/
             dlclose(handle);                                              /*We close the handle here and not earlier as earlier it was null.*/
-            return 1;
+            continue;
         }
         int result = operation(num1, num2);
         printf("%d\n", result);
